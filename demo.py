d74792085427da1d6125145c195d7c07891fe321
bluebird <diqiuonline@qq.com>
2018-02-03 03:01:40 +0800

    add hello world

---
+++ demo.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+from metasploit import module
+
+metadata = {
+    # module name
+    'name': 'metasploit python module demo ',
+    # module descript
+    'description': '''
+    hello world 
+    ''',
+    # module authors
+    'authors': [
+        'bluebird',
+    ],
+    # module write time
+    'date': '2018-02-02',
+    # bug reference
+    'references': [
+
+     ],
+    # bug type
+    'type': 'scan',
+    # module options 
+    'options': {
+        'rhost': {'type': 'address', 'description': 'The target address', 'required': True, 'default': None},
+        'rport': {'type': 'port', 'description': 'The target port', 'required': True, 'default': 80},
+     }}
+
+def run(args):
+    module.log('hello world')
+
+if __name__ == "__main__":
+    module.run(metadata, run)
